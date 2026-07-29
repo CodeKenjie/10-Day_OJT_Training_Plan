e7564de27d2af3a8e21e28779645418927076f17
KenjieTheGreate <franciskenjiem@gmail.com>
2026-07-30 07:53:55 +0800

    added: substrings

---
+++ substring.rb
@@ -0,0 +1,19 @@
+def substrings(string, dictionary)
+  clean_word = string.gsub(/[^a-zA-Z0-9\s]/, "")
+  words = clean_word.downcase.split(" ")
+  word_count = Hash.new(0)
+
+  words.each do |word|
+    dictionary.flatten.map do |dictionary_word| 
+      if word.include?(dictionary_word)
+        word_count[dictionary_word] += 1
+      end
+    end
+  end
+
+  word_count
+end
+
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
